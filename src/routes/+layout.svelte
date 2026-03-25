55caf8d922b5e85cb328acb8dbf3478814c21f7e
Manfred <walder.manfred@gmail.com>
2026-03-25 10:23:46 +0100

    add anyalytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import '../app.css';
-	import GlitterBackground from '$lib/components/GlitterBackground.svelte';
-	import LaserBeams from '$lib/components/LaserBeams.svelte';
+  import favicon from "$lib/assets/favicon.svg";
+  import "../app.css";
+  import GlitterBackground from "$lib/components/GlitterBackground.svelte";
+//   import LaserBeams from "$lib/components/LaserBeams.svelte";
+  import { dev } from "$app/environment";
+  import { injectAnalytics } from "@vercel/analytics/sveltekit";
 
-	let { children } = $props();
+  let { children } = $props();
+
+  injectAnalytics({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<title>Glitter Me Softly - Rave Birthday Party</title>
-	<meta name="description" content="You're invited to the most glittery rave birthday party ever!" />
+  <link rel="icon" href={favicon} />
+  <title>Glitter Me Softly - Rave Birthday Party</title>
+  <meta
+    name="description"
+    content="You're invited to the most glittery rave birthday party ever!"
+  />
 </svelte:head>
 
 <!-- <LaserBeams /> -->
 <GlitterBackground />
 
 <div class="relative z-2">
-	{@render children()}
+  {@render children()}
 </div>
